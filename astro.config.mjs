53c492ec3321f8b73f1f29656854766854613e1b
Anton Kovalyov <byagent@proton.me>
2025-02-17 18:03:13 -0800

    Website rework (#27)
    * First iteration on website rework
    
    * Change to blog posts
    
    * Add spacing to all articles
    
    * Update header and get involved section
    
    * Blog preview rework
    
    * new logo, bug fixes
    
    * New fonts, responsive design fixes, bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import robotsTxt from "astro-robots-txt";
 
 // See configuration guide: https://astro.build/config
 export default defineConfig({
   site: 'https://strongtownsvan.org',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
+  integrations: [mdx(), sitemap(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   output: "static"
 });
